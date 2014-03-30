374b05c7ef2726e1951c0aaae91a638262798eb6
tett23 <tett23@gmail.com>
2014-03-30 16:44:06 +0900

    インストールコマンドをまとめる

---
+++ install.sh
@@ -0,0 +1,8 @@
+ln -s dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle
+ln -s dotfiles/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
+ln -s dotfiles/.zshrc ~/.zshrc
+ln -s dotfiles/.byobu ~/.byobu
+ln -s dotfiles/.tmux ~/.tmux
+ln -s dotfiles/.gitconfig ~/.gitconfig
+ln -s dotfiles/.gitignore_global ~/.gitignore_global
