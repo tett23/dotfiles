3fa4d2c889fc21d3a0e9d92f3413d20057ad5420
tett23 <tett23@gmail.com>
2014-04-17 16:01:04 +0900

    byobuのファイルを移動

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git submodule update
 mkdir -p ~/.vim/bundle
 ln -s dotfiles/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
 ln -s dotfiles/.zshrc ~/.zshrc
-ln -s dotfiles/.byobu ~/.byobu
+ln -s dotfiles/byobu ~/.byobu
 ln -s dotfiles/tmux/powerlinerc.sh ~/.tmux-powerlinerc
 ln -s dotfiles/.gitconfig ~/.gitconfig
 ln -s dotfiles/.gitignore_global ~/.gitignore_global
