697963a301818fdde8b22c895640b115c70c7688
tett23 <tett23@gmail.com>
2014-04-17 15:58:43 +0900

    tmuxの設定は.byobu以下

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ mkdir -p ~/.vim/bundle
 ln -s dotfiles/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
 ln -s dotfiles/.zshrc ~/.zshrc
 ln -s dotfiles/.byobu ~/.byobu
-ln -s dotfiles/.tmux ~/.tmux
 ln -s dotfiles/tmux/powerlinerc.sh ~/.tmux-powerlinerc
 ln -s dotfiles/.gitconfig ~/.gitconfig
 ln -s dotfiles/.gitignore_global ~/.gitignore_global
