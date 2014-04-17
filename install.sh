86df6897b094a8fff1feb018fa8cc3335c8e5ad7
tett23 <tett23@gmail.com>
2014-04-17 11:46:39 +0900

    tmux-powerlineの設定

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ ln -s dotfiles/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
 ln -s dotfiles/.zshrc ~/.zshrc
 ln -s dotfiles/.byobu ~/.byobu
 ln -s dotfiles/.tmux ~/.tmux
+ln -s dotfiles/tmux/powerlinerc.sh ~/.tmux-powerlinerc
 ln -s dotfiles/.gitconfig ~/.gitconfig
 ln -s dotfiles/.gitignore_global ~/.gitignore_global
