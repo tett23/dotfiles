ff3c13def2b2ea4bbb97154501f4a4449d6c7aeb
tett23 <tett23@gmail.com>
2014-05-11 00:03:17 +0900

    インストールスクリプト修正

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 test -s ~/.vimrc && unlink ~/.vimrc
-ln -s dotfiles/.vimrc ~/.vimrc
+ln -s $DOTFILES/.vimrc ~/.vimrc
 test -s ~/.gvimrc && unlink ~/.gvimrc
-ln -s dotfiles/.gvimrc ~/.gvimrc
+ln -s $DOTFILES/.gvimrc ~/.gvimrc
 test $SHELL != '/bin/zsh' && chsh -s /bin/zsh
 git submodule init
 git submodule update
 mkdir -p ~/.vim/bundle
 test -s ~/.vim/bundle/neobundle.vim && unlink ~/.vim/bundle/neobundle.vim
-ln -s dotfiles/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
+ln -s $DOTFILES/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
 test -s ~/.zshrc && unlink ~/.zshrc
-ln -s dotfiles/.zshrc ~/.zshrc
-test -s ~/.byobu && unlink ~/.byobu
-ln -s dotfiles/byobu ~/.byobu
+ln -s $DOTFILES/.zshrc ~/.zshrc
 test -s ~/.tmux-powerlinerc && unlink ~/.tmux-powerlinerc
-ln -s dotfiles/tmux/powerlinerc.sh ~/.tmux-powerlinerc
+ln -s $DOTFILES/tmux/powerlinerc.sh ~/.tmux-powerlinerc
 test -s ~/.gitconfig && unlink ~/.gitconfig
-ln -s dotfiles/.gitconfig ~/.gitconfig
+ln -s $DOTFILES/.gitconfig ~/.gitconfig
 test -s ~/.gitignore_global && unlink ~/.gitignore_global
-ln -s dotfiles/.gitignore_global ~/.gitignore_global
+ln -s $DOTFILES/.gitignore_global ~/.gitignore_global
 test -s ~/.Xresources && unlink ~/.Xresources
-ln -s dotfiles/Xresources-zenburn ~/.Xresources
+ln -s $DOTFILES/Xresources-zenburn ~/.Xresources
+test -s ~/.tmux.conf && unlink ~/.tmux.conf
+ln -s $DOTFILES/tmux.conf ~/.tmux.conf
