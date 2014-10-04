0dc4016d5b2af5c3048401b868c31a4b95d14989
tett23 <tett23@gmail.com>
2014-10-05 02:36:06 +0900

    install.shなおした

--- install.sh
+++ install.sh
@@ -1,24 +1,19 @@
-test -s ~/.vimrc && unlink ~/.vimrc
-ln -s $DOTFILES/.vimrc ~/.vimrc
-test -s ~/.gvimrc && unlink ~/.gvimrc
-ln -s $DOTFILES/.gvimrc ~/.gvimrc
+if [ -z "$DOTFILES" ]; then
+  export DOTFILES=$HOME/dotfiles
+fi
+
+ln -s -f $DOTFILES/.vimrc $HOME/.vimrc
+ln -s -f $DOTFILES/.gvimrc $HOME/.gvimrc
 test $SHELL != '/bin/zsh' && chsh -s /bin/zsh
 git submodule init
 git submodule update
-mkdir -p ~/.vim/bundle
-test -s ~/.vim/bundle/neobundle.vim && unlink ~/.vim/bundle/neobundle.vim
-ln -s $DOTFILES/vim/neobundle.vim/ ~/.vim/bundle/neobundle.vim
-test -s ~/.zshrc && unlink ~/.zshrc
-ln -s $DOTFILES/.zshrc ~/.zshrc
-test -s ~/.tmux-powerlinerc && unlink ~/.tmux-powerlinerc
-ln -s $DOTFILES/tmux/powerlinerc.sh ~/.tmux-powerlinerc
-test -s ~/.gitconfig && unlink ~/.gitconfig
-ln -s $DOTFILES/.gitconfig ~/.gitconfig
-test -s ~/.gitignore_global && unlink ~/.gitignore_global
-ln -s $DOTFILES/.gitignore_global ~/.gitignore_global
-test -s ~/.Xresources && unlink ~/.Xresources
-ln -s $DOTFILES/Xresources-zenburn ~/.Xresources
-test -s ~/.tmux.conf && unlink ~/.tmux.conf
-ln -s $DOTFILES/tmux.conf ~/.tmux.conf
+mkdir -p $HOME/.vim/bundle
+ln -nsf $DOTFILES/vim/neobundle.vim/ $HOME/.vim/bundle/neobundle.vim
+ln -nsf $DOTFILES/.zshrc $HOME/.zshrc
+ln -nsf $DOTFILES/tmux/powerlinerc.sh $HOME/.tmux-powerlinerc
+ln -nsf $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -nsf $DOTFILES/.gitignore_global $HOME/.gitignore_global
+ln -nsf $DOTFILES/Xresources-zenburn $HOME/.Xresources
+ln -nsf $DOTFILES/tmux.conf $HOME/.tmux.conf
 
 $DOTFILES/zsh/fzf/install
