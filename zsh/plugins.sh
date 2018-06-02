b44d7aa4deb691fe0d3a3ab1148010f3e6602e39
tett23 <tett23@gmail.com>
2018-06-03 02:32:45 +0900

    Remove unncessary files

--- zsh/plugins.sh
+++
@@ -1,1 +0,0 @@
-plugins=(git ruby bundler rails emoji-clock autoenv)
--- zsh/prezto.sh
+++
@@ -1,4 +0,0 @@
-setopt EXTENDED_GLOB
-for rcfile in "${DOTIFLES}"/prezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${DOTFILES}/prezto/.${rcfile:t}"
-done
