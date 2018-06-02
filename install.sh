3651e175928c91e20c4dfeec7caeaf70ba5059c5
tett23 <tett23@gmail.com>
2018-06-03 03:01:32 +0900

    Remove dot prefix

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ if [ -z "$XDG_CONFIG_HOME" ]; then
   export XDG_CONFIG_HOME=$HOME/.config
 fi
 
-ln -s -f $DOTFILES/.vimrc $HOME/.vimrc
-ln -s -f $DOTFILES/.nvimrc $HOME/.nvimrc
-ln -s -f $DOTFILES/.gvimrc $HOME/.gvimrc
+ln -nsf $DOTFILES/vimrc $HOME/.vimrc
+ln -nsf $DOTFILES/nvimrc $HOME/.nvimrc
+ln -nsf $DOTFILES/gvimrc $HOME/.gvimrc
 ln -nsf $DOTFILES/vim $XDG_CONFIG_HOME/nvim
 ln -nsf $DOTFILES/vim $HOME/.vim
 mkdir -p $HOME/.vimbackup
@@ -16,10 +16,10 @@ mkdir -p $HOME/.vimbackup
 git submodule update --init --recursive
 
 ln -nsf $DOTFILES/zshenv $HOME/.zshenv
-ln -nsf $DOTFILES/.zshrc $HOME/.zshrc
+ln -nsf $DOTFILES/zshrc $HOME/.zshrc
 ln -nsf $DOTFILES/tmux/powerlinerc.sh $HOME/.tmux-powerlinerc
-ln -nsf $DOTFILES/.gitconfig $HOME/.gitconfig
-ln -nsf $DOTFILES/.gitignore_global $HOME/.gitignore_global
+ln -nsf $DOTFILES/gitconfig $HOME/.gitconfig
+ln -nsf $DOTFILES/gitignore_global $HOME/.gitignore_global
 ln -nsf $DOTFILES/Xresources-zenburn $HOME/.Xresources
 ln -nsf $DOTFILES/tmux.conf $HOME/.tmux.conf
 ln -nsf $DOTFILES/exenv $HOME/.exenv
