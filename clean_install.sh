296941f4809f7be777ceaae85b2d5a3f3e3db56b
tett23 <tett23@gmail.com>
2018-11-21 20:06:24 +0900

    Add clean install script

---
+++ clean_install.sh
@@ -0,0 +1,5 @@
+chsh -s /usr/local/bin/zsh
+brew install gnupg2
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
