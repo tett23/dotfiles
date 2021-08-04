cc9042fc4650dfaf86f403297bcb28571aa8149b
tett23 <tett23@gmail.com>
2021-08-04 19:26:28 +0900

    Use Volta

--- clean_install.sh
+++ clean_install.sh
@@ -1,7 +1,16 @@
 chsh -s /usr/local/bin/zsh
 brew install gnupg2
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+# homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-curl https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L > eskk/SKK-JISYO.L
+
+# volta
+curl https://get.volta.sh | bash
+
+# rustup
 curl https://sh.rustup.rs -sSf | sh
+
+# SKK jisyo
+curl https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L > eskk/SKK-JISYO.L
+
--- zsh/anyenv.sh
+++ zsh/anyenv.sh
@@ -1,5 +1,3 @@
-source ~/.nvm/nvm.sh
-
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
 export PATH=$DOTFILES/exenv/bin:$PATH
@@ -7,3 +5,7 @@ echo 'eval "$(exenv init -)"' >> ~/.bash_profile
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 source ~/.rvm/scripts/rvm
+
+# volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
