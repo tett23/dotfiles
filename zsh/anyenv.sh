4eae87c2648c9f2a2270f91c7f443564ea83f4cf
tett23 <tett23@gmail.com>
2022-06-26 23:37:44 +0900

    Add rbenv settings

--- zsh/anyenv.sh
+++ zsh/anyenv.sh
@@ -1,11 +1,8 @@
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
-export PATH=$DOTFILES/exenv/bin:$PATH
-echo 'eval "$(exenv init -)"' >> ~/.bash_profile
-
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-source ~/.rvm/scripts/rvm
-
 # volta
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+# rbenv
+eval "$(rbenv init - zsh)"
